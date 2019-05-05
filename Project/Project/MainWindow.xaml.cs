f8f7589a591ff640aa58a0017a6f57e0eda1228d
Shark_vil <shark_vil@mail.ru>
2019-05-05 23:03:35 +0300

    Added simple structure

--- Project/Project/MainWindow.xaml.cs
+++ Project/Project/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,91 @@ namespace Project
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Model.Data> allowDublicateButtons = new List<Model.Data>();
+        List<Model.Data> allowButtons = new List<Model.Data>();
+        List<Model.DataNumber> allowButtonsIndex = new List<Model.DataNumber>();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Происходит при нажатии на кнопку в твблице
+        /// </summary>
+        private void _Action_DataGrid_ButtonElement_AddNumber(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// Добавляет кнопку в таблицу
+        /// </summary>
+        private void _Action_Button_AddTextToDataGrid_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверка на то, что поле с текстом не пустое
+            if (_Action_TextBox_UserText.Text.Length != 0)
+            {
+                bool isExists_ButtonData = false;
+                int newIndex = 0;
+
+                for (int i = 0; i < allowButtonsIndex.Count; i++)
+                {
+                    Model.DataNumber ButtonData = allowButtonsIndex[i];
+                    if (ButtonData.Text == _Action_TextBox_UserText.Text)
+                    {
+                        isExists_ButtonData = true;
+                        newIndex = ButtonData.Index = ButtonData.Index++;
+                        break;
+                    }
+                }
+
+                if (!isExists_ButtonData)
+                    allowButtonsIndex.Add(new Model.DataNumber { Index = 0, Text = _Action_TextBox_UserText.Text });
+
+
+
+                bool isExists_Button = false;
+
+                for (int i = 0; i < allowButtons.Count; i++)
+                {
+                    Model.Data Button = allowButtons[i];
+                    if (Button.ButtonContent == _Action_TextBox_UserText.Text)
+                    {
+                        MessageBox.Show(Button.ButtonContent);
+                        isExists_Button = true;
+                        allowDublicateButtons = allowButtons.GetRange(0, allowButtons.Count);
+                        allowDublicateButtons[i].ButtonContent = _Action_TextBox_UserText.Text + $" {newIndex}";
+                        MessageBox.Show(allowDublicateButtons[i].ButtonContent);
+                        MessageBox.Show(Button.ButtonContent);
+                        break;
+                    }
+                }
+
+                if (!isExists_Button)
+                    allowButtons.Add(new Model.Data { Id = allowButtons.Count + 1,
+                        ButtonContent = _Action_TextBox_UserText.Text });
+
+
+
+                Task.Factory.StartNew(async () =>
+                {
+                    await Application.Current.Dispatcher.InvokeAsync(() =>
+                    {
+                        _Action_DataGrid_ButtonsList.ItemsSource = null;
+                        _Action_DataGrid_ButtonsList.ItemsSource = allowDublicateButtons;
+                    });
+                });
+            }
+        }
+
+        /// <summary>
+        /// Очищает таблицу
+        /// </summary>
+        private void _Action_Button_ClearToDataGrid_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Project/Project/Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Project
+{
+    class Model
+    {
+        public class Data
+        {
+            public int Id { get; set; }
+            public string ButtonContent { get; set; }
+        }
+
+        public class DataNumber
+        {
+            public string Text { get; set; }
+            public int Index { get; set; }
+        }
+    }
+}
